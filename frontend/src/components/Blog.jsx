25b9647bc8eff7d542e5d3a98d97b500066bc63e
Inayat Ullah Khan <inayatullahkhan0212@gmail.com>
2025-04-15 18:38:54 +0530

    add utils.js

--- frontend/src/components/Blog.jsx
+++ frontend/src/components/Blog.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import { api_url } from './utils';
 
 
 const Blog = ({data}) => {
@@ -8,7 +9,7 @@ const Blog = ({data}) => {
   return (
     <>
       <div onClick={()=>{navigate(`/blog/${data._id}`)}} className="blog">
-        <img className='w-full h-[60%] rounded-lg mb-2' src={`http://localhost:3000/uploads/${data.image}`} alt="" />
+        <img className='w-full h-[60%] rounded-lg mb-2' src={`${api_url}/uploads/${data.image}`} alt="" />
         <h3>{data.title}</h3>
         <p className='text-[gray] text-[14px] w-[90%] line-clamp-3'>{data.desc}</p>
       </div>
---
+++ frontend/src/components/utils.js
@@ -0,0 +1,1 @@
+export const api_url = import.meta.env.API_URL
